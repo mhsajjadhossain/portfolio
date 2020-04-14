49c8bc705a683ef864e2fc8a4e4f5c31289c1183
mhsajjadhossain <mail.mhsajjad@gmail.com>
2020-04-14 18:37:16 +0600

    final sub

--- assets/js/swiper-active.js
+++ assets/js/swiper-active.js
@@ -1,2 +1,7 @@
-
-var swiper = new Swiper('.swiper-container', {direction: 'horizontal',slidesPerView: 2,spaceBetween: 30,mousewheel: true,});
+var swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    mousewheel: true,
+    speed:500,
+});
