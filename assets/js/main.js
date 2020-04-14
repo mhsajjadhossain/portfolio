4df1317955b6d3c348420d61d627a7c85df2b43b
mhsajjadhossain <mail.mhsajjad@gmail.com>
2020-04-14 14:07:15 +0600

    solve body scroll solve

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,9 +49,11 @@ $(function () {
                     scrollTop: 0,
                 }, 200);
 
-                
+                $("body").css("overflow-y","hidden")
             })
-            
+            $(".close_btn").on('click',function(){
+                $("body").css("overflow-y","scroll")
+            })
 
         } else {
             $('.swiper-wrapper').addClass('d-flex');
